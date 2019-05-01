fd32ee543b22cf72b51ab02c8344ea4061219eec
Sean Lange <48290603+Langesae@users.noreply.github.com>
2019-05-01 10:18:20 -0500

    Add files via upload

--- Data/wrapper.py
+++ Data/wrapper.py
@@ -1,16 +1,20 @@
-import stockpredictionTest as prediction
+import stockprediction as prediction
+import os
 
-
-# input vars
 days = 1
 stock = ''
 save = False
 plot = True
 stocks = ['AXP', 'BAC', 'BRK.B', 'CB', 'C', 'CME', 'GS', 'JPM', 'USB', 'WFC']
+buy = 5
+sell = 5
+FileName = os.getcwd()
 
 while True:
 	use = input("Do you want to run the network. Y/N? ")
 	if use == 'Y':
+		buy = input("What is the percentage of gain for a buy? ")
+		buy = input("What is the percentage of loss for a sell? ")
 		print("Plug in Laptop network could overload internal battery")
 		print(stocks)
 		stock = input("What stock above would you like to use? ")
@@ -18,4 +22,13 @@ while True:
 			print("Not in list. Try again. ")
 			stock = input("What stock above would you like to use? ")
 		prediction.main(stock)
+		FileName += '/Results/' + stock + '-Prediction_vs_Actual'
+	elif use == 'N':
+		break
+		
+		
+		
+
+
+	
 	
