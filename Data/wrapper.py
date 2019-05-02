0fb5c49a461ccf7480c556e51e831c27bc40c5d4
Sean Lange <48290603+Langesae@users.noreply.github.com>
2019-05-02 18:43:14 -0500

    Add files via upload

--- Data/wrapper.py
+++ Data/wrapper.py
@@ -5,19 +5,37 @@ import pandas as pd
 import xlrd
 
 stock = ''
-save = False
-plot = True
 stocks = ['AXP', 'BAC', 'BRK.B', 'CB', 'C', 'CME', 'GS', 'JPM', 'USB', 'WFC']
-buy = 5
-sell = 5
+buyPre = 5
+sellPre = 5
+percentage = 0 
+buy = False
+sell = False
+hold = False
 FileName = os.getcwd()
 
+def calc (file, buyPre, sellPre, buy , sell, hold):
+	with open(file) as f:
+		reader = csv.reader(f)
+		a = list(reader)
+		prediction = float(a[-1][-1])
+		actual = float(a[-1][-2])
+		difference = prediction - actual
+		percentage = (difference / actual) * 100
+		if percentage >= buyPre:
+			buy = True
+		elif percentage <= (sellPre * -1):
+			sell = True
+		else:
+			hold = True
+		return percentage, buy, sell, hold
+		
 
 while True:
 	use = input("Do you want to run the network. Y/N? ")
 	if use == 'Y':
-		buy = input("What is the percentage of gain for a buy? ")
-		buy = input("What is the percentage of loss for a sell? ")
+		buyPre = int(input("What is the percentage of gain for a buy? "))
+		sellPre = int(input("What is the percentage of loss for a sell? "))
 		print("Plug in Laptop network could overload internal battery")
 		print(stocks)
 		stock = input("What stock above would you like to use? ")
@@ -26,8 +44,13 @@ while True:
 			stock = input("What stock above would you like to use? ")
 		prediction.main(stock)
 		FileName += '/Results/' + stock + '_Optimizer_Initializer_CostFunction_FinalPredictions.csv' 
-		file = pd.read_excel(FileName)
-		
+		percentage, buy, sell, hold = (calc(FileName, buyPre, sellPre, buy, sell, hold))
+		if sell == True:
+			print("We advise you to sell " + stock + ".")
+		elif buy == True:
+			print("We advise you to buy " + stock + ".")
+		elif hold == True:
+			print("We advise you to hold " + stock + ".")
 	elif use == 'N':
 		break
 		
